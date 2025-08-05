2e4fc61085e52dd6afa00dd9e37ef7cdd845d9e3
Ashish <ashishchauhan@a1apps.co>
2025-08-05 13:18:06 +0530

    updated firebase, appmetrica, clarity

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "A1AppSDK",
     platforms: [
-           .iOS(.v14), // Adjust platform version as needed
+           .iOS(.v15), // Adjust platform version as needed
            .macOS(.v12),
        ],
     products: [
@@ -17,14 +17,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.9.1"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.9.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "12.0.0"),
         .package(url: "https://github.com/facebook/facebook-ios-sdk.git", exact: "18.0.0"),
-        .package(url: "https://github.com/appmetrica/appmetrica-sdk-ios", exact: "5.8.1"),
+        .package(url: "https://github.com/appmetrica/appmetrica-sdk-ios", exact: "5.11.0"),
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "11.2.0"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", exact: "5.0.2"),
         .package(url: "https://github.com/BeauNouvelle/ShimmerSwift", exact: "2.2.0"),
-        .package(url: "https://github.com/helpscout/beacon-ios-sdk", exact: "3.0.1"),
-        .package(url: "https://github.com/microsoft/clarity-apps", exact: "2.1.2"),
+        .package(url: "https://github.com/microsoft/clarity-apps", exact: "3.0.12"),
         
     ],
     targets: [
@@ -42,7 +41,6 @@ let package = Package(
                 .product(name: "FirebaseAppDistribution-Beta", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseDatabase", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseDynamicLinks", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseFunctions", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseInAppMessaging-Beta", package: "firebase-ios-sdk"),
@@ -65,8 +63,6 @@ let package = Package(
                 .product(name: "SwiftyJSON", package: "SwiftyJSON"),
                 
                 .product(name: "ShimmerSwift", package: "ShimmerSwift"),
-                
-                .product(name: "Beacon-iOS", package: "beacon-ios-sdk"),
                 
                 .product(name: "Clarity", package: "clarity-apps"),
 
