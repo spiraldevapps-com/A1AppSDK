baea50b3bec31bac5084cc578660dbd1ce96d60e
Sagar Singh <sagar@a1apps.co>
2025-08-05 18:17:39 +0530

    firebase updated to version 11.15.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.9.1"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.9.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.15.0"),
         .package(url: "https://github.com/facebook/facebook-ios-sdk.git", exact: "18.0.0"),
         .package(url: "https://github.com/appmetrica/appmetrica-sdk-ios", exact: "5.8.1"),
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "11.2.0"),
