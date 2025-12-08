be9235b7093a8c86bc3fad36c256769b1bb2346d
Sagar Singh <sagar@a1apps.co>
2025-12-08 18:07:13 +0530

    firebase updated to version 12.6.0 and meta updated to 18.0.1

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.9.1"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.15.0"),
-        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", exact: "18.0.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "12.6.0"),
+        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", exact: "18.0.1"),
         .package(url: "https://github.com/appmetrica/appmetrica-sdk-ios", exact: "5.8.1"),
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "11.2.0"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", exact: "5.0.2"),
@@ -42,7 +42,6 @@ let package = Package(
                 .product(name: "FirebaseAppDistribution-Beta", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseDatabase", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseDynamicLinks", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseFunctions", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseInAppMessaging-Beta", package: "firebase-ios-sdk"),
